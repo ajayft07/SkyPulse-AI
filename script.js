20f6ed9d5f24278636195addde6845d3faf64152
ajayft07 <ajaykingson@gmail.com>
2026-06-14 12:01:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
+const apiKey = "PASTE_YOUR_API_KEY_HERE";
 
+async function getWeather(){
+
+const city = document.getElementById("cityInput").value;
+
+if(city === ""){
+alert("Enter a city name");
+return;
+}
+
+const url =
+`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+try{
+
+const response = await fetch(url);
+const data = await response.json();
+
+document.getElementById("city").innerText =
+data.name;
+
+document.getElementById("temperature").innerText =
+Math.round(data.main.temp) + "°C";
+
+document.getElementById("description").innerText =
+data.weather[0].description;
+
+document.getElementById("humidity").innerText =
+data.main.humidity + "%";
+
+document.getElementById("wind").innerText =
+data.wind.speed + " km/h";
+
+}
+catch(error){
+
+alert("City not found");
+
+}
+
+}
